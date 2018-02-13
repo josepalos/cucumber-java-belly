b0ead9eb6431c4a1666ab18087232ad073f27d22
Josep <josep.alos.pascual@hotmail.com>
2018-02-13 17:59:56 +0100

    Added when conditions for the cucumber tests.

--- src/test/java/mybelly/steps/BellyCukesSteps.java
+++ src/test/java/mybelly/steps/BellyCukesSteps.java
@@ -2,6 +2,7 @@ package mybelly.steps;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import mybelly.Belly;
 
 import static org.hamcrest.core.Is.is;
@@ -15,6 +16,16 @@ public class BellyCukesSteps {
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         belly = new Belly();
         belly.eatCukes(cukes);
+    }
+
+    @When("^I don't wait$")
+    public void I_dont_wait() throws  Throwable {
+        waitingTime = 0;
+    }
+
+    @When("^I wait (\\d+) hour$")
+    public void I_wait_hour(int hour) throws Throwable {
+        waitingTime = hour;
     }
 
     @Then("^My belly should (?:be )?\"([^\"]*)\"$")
