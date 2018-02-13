a8c93414ffd00e0ced5b8591169a4012ba73f0a4
Josep <josep.alos.pascual@hotmail.com>
2018-02-13 18:41:58 +0100

    Implemented belly.

--- src/main/java/mybelly/Belly.java
+++ src/main/java/mybelly/Belly.java
@@ -5,9 +5,12 @@ public class Belly {
     private int cukes;
 
     public void eatCukes(int cukes) {
+        this.cukes = cukes;
     }
 
     public String getSound(int waitingTime) {
-        return "";
+        if(waitingTime == 0) return "silent";
+        if(this.cukes <= 20) return "silent";
+        else return "growl";
     }
 }
